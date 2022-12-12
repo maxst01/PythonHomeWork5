5349534c272efdfeb95ace744d72f8c40638d15f
Assauit <maxst01@list.ru>
2022-12-12 11:16:43 +0700

    FULL

--- PythonHomeWork5.py
+++ PythonHomeWork5.py
@@ -151,3 +151,92 @@
 #     print(f"Выиграл {player1}")
 # else:
 #     print(f"Выиграл {player2}")
+
+
+#####################################################
+# board = list(range(1,10))
+
+# def draw_board(board):
+#     print ("-" * 13)
+#     for i in range(3):
+#         print ("|", board[0+i*3], "|", board[1+i*3], "|", board[2+i*3], "|")
+#         print ("-" * 13)
+
+# def take_input(player_token):
+#     valid = False
+#     while not valid:
+#         player_answer = input("Куда поставим " + player_token+"? ")
+#         try:
+#             player_answer = int(player_answer)
+#         except:
+#             print ("Некорректный ввод. Вы уверены, что ввели число?")
+#             continue
+#         if player_answer >= 1 and player_answer <= 9:
+#             if (str(board[player_answer-1]) not in "XO"):
+#                 board[player_answer-1] = player_token
+#                 valid = True
+#             else:
+#                 print ("Эта клеточка уже занята")
+#         else:
+#             print ("Некорректный ввод. Введите число от 1 до 9 чтобы походить.")
+
+
+#############################################
+# def coding(txt):
+#     count = 1
+#     res = ''
+#     for i in range(len(txt)-1):
+#         if txt[i] == txt[i+1]:
+#             count += 1
+#         else:
+#             res = res + str(count) + txt[i]
+#             count = 1
+#     if count > 1 or (txt[len(txt)-2] != txt[-1]):
+#         res = res + str(count) + txt[-1]
+#     return res
+
+# def decoding(txt):
+#     number = ''
+#     res = ''
+#     for i in range(len(txt)):
+#         if not txt[i].isalpha():
+#             number += txt[i]
+#         else:
+#             res = res + txt[i] * int(number)
+#             number = ''
+#     return res
+
+
+# s = input("Введите текст для кодировки: ")
+# print(f"Текст после кодировки: {coding(s)}")
+# print(f"Текст после дешифровки: {decoding(coding(s))}")
+
+# def check_win(board):
+#     win_coord = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+#     for each in win_coord:
+#         if board[each[0]] == board[each[1]] == board[each[2]]:
+#             return board[each[0]]
+#     return False
+
+# def main(board):
+#     counter = 0
+#     win = False
+#     while not win:
+#         draw_board(board)
+#         if counter % 2 == 0:
+#             take_input("X")
+#         else:
+#             take_input("O")
+#         counter += 1
+#         if counter > 4:
+#             tmp = check_win(board)
+#             if tmp:
+#                 print (tmp, "выиграл!")
+#                 win = True
+#                 break
+#         if counter == 9:
+#             print ("Ничья!")
+#             break
+#     draw_board(board)
+
+# main(board)
